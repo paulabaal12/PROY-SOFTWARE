4e624a2f494a4fd1bf5e1f5f0065b5011da09a11
Carlos Valladares <val221164@uvg.edu.gt>
2024-04-04 18:44:45 -0600

    Crear un posible App.jsx implementando las 3 páginas anteriores creadas.
    Falta de probar, estoy teniendo problemas con Meteor...

--- Scrum/software/imports/ui/App.jsx
+++ Scrum/software/imports/ui/App.jsx
@@ -18,3 +18,61 @@ export const App = () => {
     </div>
   );
 };
+
+/* 
+import React, { useState } from 'react';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+import HomePage from './HomePage'; // Lo que le toca crear a Brandon
+import PaymentMethodPage from './PaymentMethodPage';
+import PaymentSummaryPage from './PaymentSummaryPage';
+import ShoppingCartPage from './ShoppingCartPage';
+
+export const App = () => {
+  const [currentPage, setCurrentPage] = useState('login');
+  const [transactionId, setTransactionId] = useState(null);
+
+  const goToRegister = () => setCurrentPage('register');
+  const goToLogin = () => setCurrentPage('login');
+  const goToHome = () => setCurrentPage('home');
+  const goToShoppingCart = () => setCurrentPage('shoppingCart');
+  const goToPaymentMethod = () => setCurrentPage('paymentMethod');
+  const goToPaymentSummary = (id) => {
+    setTransactionId(id); // Aquí se debe cambiar por el verdadero ID de transacción obtenido tras el proceso de pago
+    setCurrentPage('paymentSummary');
+  };
+
+  const confirmPaymentAndGoHome = () => {
+    // Lógica para manejar la confirmación de pago
+    console.log('Pago confirmado con ID:', transactionId);
+    setCurrentPage('home'); // Después de confirmar el pago, redirige al usuario al inicio
+  };
+
+  const renderCurrentPage = () => {
+    switch (currentPage) {
+      case 'login':
+        return <LoginPage setShowRegister={goToRegister} />;
+      case 'register':
+        return <RegisterPage goBack={goToLogin} />;
+      case 'home':
+        return <HomePage goToShoppingCart={goToShoppingCart} />;
+      case 'shoppingCart':
+        return <ShoppingCartPage onCheckout={goToPaymentMethod} />;
+      case 'paymentMethod':
+        return <PaymentMethodPage onPayment={goToPaymentSummary} />;
+      case 'paymentSummary':
+        return <PaymentSummaryPage transactionId={transactionId} onConfirmPayment={confirmPaymentAndGoHome} />;
+      default:
+        return <LoginPage setShowRegister={goToRegister} />;
+    }
+  };
+
+  return (
+    <div>
+      {renderCurrentPage()}
+    </div>
+  );
+};
+
+export default App;
+*/
