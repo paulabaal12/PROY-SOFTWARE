d52574469e10b4efaa68a32aae334cdb7b27b18d
G2309 <cru22779@uvg.edu.gt>
2024-04-17 15:18:29 -0600

    Added experimental Docker+Postgresql database

---
+++ Scrum/software/db/schema.sql
@@ -0,0 +1,16 @@
+
+CREATE DATABASE IF NOT EXISTS users_database;
+
+\c users_database;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	dpi VARCHAR(32) NOT NULL,
+	location VARCHAR(255) NOT NULL,
+	profile_picture BYTEA,
+	has_agreed_to_privacy_policy BOOLEAN NOT NULL,
+	enable_2fa BOOLEAN DEFAULT FALSE
+);
