d625ee9430b29e1789dabe698b9d800d929d475e
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2024-04-19 01:49:48 -0600

    ajustes

--- Scrum/software/imports/ui/HomePage.jsx
+++ Scrum/software/imports/ui/HomePage.jsx
@@ -18,12 +18,12 @@ const products = [
 ];
 
 const carouselImages = [
-  { imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51bm38JKdKL._SX600_.jpg', link: '/ruta1' },
-  { imageUrl: 'https://m.media-amazon.com/images/I/81vaif8cb6L.__AC_SX300_SY300_QL70_FMwebp_.jpg', link: '/ruta2' },
-  { imageUrl: 'https://i5.walmartimages.com/seo/Restored-Apple-iPhone-14-Plus-Carrier-Unlocked-128GB-Starlight-MQ3T3LL-A-Refurbished_82bd983b-d7bd-41ce-b6cb-b7d1e550cb85.add6d9bd949bab209f32d5be9ac1d0f9.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/ruta1' },
-  { imageUrl: 'https://i5.walmartimages.com/seo/Ice-Breakers-Ice-Cubes-Peppermint-Sugar-Free-Chewing-Gum-Bottle-3-24-oz-40-Pieces_5c3a2719-7ab3-478d-8700-52bda92095d9.328047a8e4e11f9761cb3a9fadab7fea.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/ruta2' },
-  { imageUrl: 'https://www.sephora.com/productimages/sku/s2761971-main-zoom.jpg?imwidth=612', link: '/ruta1' },
-  { imageUrl: 'https://i5.walmartimages.com/seo/Spring-Valley-Immune-Health-Non-GMO-Vitamin-C-Vegetarian-Gummies-Orange-250-mg-150-Count_91797f36-d7bc-4b18-969f-a2cd7781437b.b2688b900c7690f0e4eb0264e8324914.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/ruta2' },
+  { imageUrl: 'https://i5.walmartimages.com/seo/Mainstays-Alessandra-Matte-Black-12-Piece-Stoneware-Dinnerware-Set_a06eabea-1ba7-4a41-abbc-63151bf7c1b0.9fb529e6c752dbc2ef2efa717418a28f.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/categoria1' },
+  { imageUrl: 'https://i5.walmartimages.com/seo/Doritos-Flavored-Tortilla-Chips-Flamin-Hot-Limon-9-25-oz-Bag_0454f30f-2be3-42ea-9ab9-c2ea09590311.9b898b540fdf6ae7019abf2b0b45f19b.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/categoria2' },
+  { imageUrl: 'https://i5.walmartimages.com/seo/Carote-Nonstick-Pots-and-Pans-Set-8-Pcs-Induction-Kitchen-Cookware-Sets-Beige-Granite_e4183757-3e78-4321-a9ad-3123804e77ad.98cb62d1e47bdc84f235cbdcfafac1db.png?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/categoria3' },
+  { imageUrl: 'https://i5.walmartimages.com/seo/Sunny-Fun-Bounce-House-Nylon-Bouncy-House-for-Kids-Outdoor-with-Toddler-Slide_c653007e-0f42-4b95-813d-73b50f3064af.385c1063fcbdf8a36aff24733aeea5c3.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/categoria4' },
+  { imageUrl: 'https://www.sephora.com/productimages/sku/s2761971-main-zoom.jpg?imwidth=612', link: '/categoria5' },
+  { imageUrl: 'https://i5.walmartimages.com/seo/Spring-Valley-Immune-Health-Non-GMO-Vitamin-C-Vegetarian-Gummies-Orange-250-mg-150-Count_91797f36-d7bc-4b18-969f-a2cd7781437b.b2688b900c7690f0e4eb0264e8324914.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF', link: '/categoria6' },
 ];
 
 const HomePage = () => {
