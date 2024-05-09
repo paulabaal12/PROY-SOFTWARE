e4094a7c7e9c38f84da3b8bf8a0d9865b077d823
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2024-05-08 20:09:01 -0600

    Fix: Reparación de pantalla
    Se reparo la pantalla, se tien problemas
    Es necesario instalar:
     - Instala Material-UI:
    npm install @mui/material @emotion/react @emotion/styled
    
     - Instala Recharts:
    npm install recharts
    
    meteor npm install --save @material-ui/core

--- Scrum/software/imports/ui/Components/Screens/visualizador_pagos.jsx
+++ Scrum/software/imports/ui/Components/Screens/visualizador_pagos.jsx
@@ -1,7 +1,8 @@
-// visualizador_pagos.jsx
-import React, { useState } from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './css/ShoppingCartPage.css';
+import Header from '../../Header';
+import Footer from '../../Footer';
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Select, MenuItem } from '@material-ui/core';
 import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
 
@@ -18,23 +19,27 @@ const transactionsData = [
   { id: 8, monto: 3482.01, fechaInicio: '01/11/2022', fechaFin: '26/07/2021', medioPago: 'Mastercard', estado: 'Cancelado' },
   { id: 9, monto: 399.6, fechaInicio: '18/02/2022', fechaFin: '16/10/2021', medioPago: 'Dinero', estado: 'Completado' },
   { id: 10, monto: 4898.28, fechaInicio: '05/10/2023', fechaFin: '26/12/2023', medioPago: 'Mastercard', estado: 'Completado' }
-];
-
-// Colores para estados de las transacciones
-const paymentStatusColors = {
+ ];
+ 
+ 
+ // Colores para estados de las transacciones
+ const paymentStatusColors = {
   Completado: "#0088FE",
   Pendiente: "#00C49F",
   Cancelado: "#FFBB28"
-};
-
-// Función para agrupar los datos por estado y por método de pago
-const groupBy = (key, data) => data.reduce((acc, item) => {
+ };
+ 
+ 
+ // Función para agrupar los datos por estado y por método de pago
+ const groupBy = (key, data) => data.reduce((acc, item) => {
   acc[item[key]] = acc[item[key]] || { name: item[key], value: 0 };
   acc[item[key]].value += 1;
   return acc;
-}, {});
-
-const Dashboard = () => {
+ }, {});
+ 
+const ShoppingCartPage = () => {
+  const navigate = useNavigate();
+  
   const [selectedState, setSelectedState] = useState('');
 
   const handleStateChange = (event) => {
@@ -49,102 +54,163 @@ const Dashboard = () => {
   // Datos agrupados para las gráficas
   const dataGroupedByState = Object.values(groupBy('estado', transactionsData));
   const dataGroupedByPaymentMethod = Object.values(groupBy('medioPago', transactionsData));
-
-  return (
-    <div>
-      <Paper style={{ margin: 16, padding: 16 }}>
-        {/* Filtros y Tabla de Transacciones */}
-        <Select
-          value={selectedState}
-          onChange={handleStateChange}
-        >
-          <MenuItem value="">Todos</MenuItem>
-          <MenuItem value="Pendiente">Pendiente</MenuItem>
-          <MenuItem value="Cancelado">Cancelado</MenuItem>
-          <MenuItem value="Completado">Completado</MenuItem>
-        </Select>
-
-        {/* Tabla de Transacciones */}
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>ID Venta</TableCell>
-                <TableCell>Monto</TableCell>
-                <TableCell>Fecha Inicio</TableCell>
-                <TableCell>Fecha Fin</TableCell>
-                <TableCell>Medio Pago</TableCell>
-                <TableCell>Estado</TableCell>
-              </TableRow>
-            </TableHead>
-            {/* ... aquí iría la tabla de transacciones y filtros  ... */}
-
-            <TableBody>
-              {filteredTransactions.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.monto}</TableCell>
-                  <TableCell>{row.fechaInicio}</TableCell>
-                  <TableCell>{row.fechaFin}</TableCell>
-                  <TableCell>{row.medioPago}</TableCell>
-                  <TableCell>{row.estado}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+  // Lista temporal de productos
 
 
-        {/* Gráficas de Pie */}
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          {/* Gráfica de Pie por Estados */}
-          <PieChart width={400} height={400}>
-            <Pie
-              data={dataGroupedByState}
-              cx={200}
-              cy={200}
-              labelLine={false}
-              label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {dataGroupedByState.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={paymentStatusColors[entry.name]} />
-              ))}
-            </Pie>
-            <Legend />
-            <Tooltip />
-          </PieChart>
 
-          {/* Gráfica de Pie por Métodos de Pago */}
-          <PieChart width={400} height={400}>
-            <Pie
-              data={dataGroupedByPaymentMethod}
-              cx={200}
-              cy={200}
-              labelLine={false}
-              label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {dataGroupedByPaymentMethod.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={paymentStatusColors[entry.name] || "#8884d8"} />
-              ))}
-            </Pie>
-            <Legend />
-            <Tooltip />
-          </PieChart>
-        </div>
-      </Paper>
-    </div>
-  );
-};
+ 
+  // Inicializar estado del carrito directamente con productos temporales
+  //const [cartItems, setCartItems] = useState(initialProducts);
 
-export const visualizador_pagos = () => (
-  <div>
-    <h1>Gestor de pagos</h1>
-    <Dashboard />
+  // Guardar el carrito en almacenamiento local cuando se actualiza
+  //useEffect(() => {
+  //  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  //}, [cartItems]);
+
+  //const handleRemove = (itemId) => {
+  //  const newCartItems = cartItems.filter(item => item.id !== itemId);
+  //  setCartItems(newCartItems);
+  //};
+/*  
+  const handleChangeQuantity = (itemId, delta) => {
+    const newCartItems = cartItems.map(item => {
+      if (item.id === itemId) {
+        const newQuantity = item.quantity + delta;
+        return { ...item, quantity: newQuantity > 0 ? newQuantity : 1 };
+      }
+      return item;
+    });
+    setCartItems(newCartItems);
+  };
+
+  const handleCheckout = () => {
+    navigate('/payment-summary', { state: { cartItems } });
+  };
+
+  const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+*/
+
+return (
+  /*
+  
+
+  <div className="shopping-cart">
+    <Header />
+    <h1>Carrito de Compras</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Cantidad</th>
+          <th>Precio Unitario</th>
+          <th>Total</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+
+    </table>
   </div>
 );
+  */
+
+
+
+<div>
+<Paper style={{ margin: 16, padding: 16 }}>
+  {/* Filtros y Tabla de Transacciones */}
+  <Select
+    value={selectedState}
+    onChange={handleStateChange}
+  >
+    <MenuItem value="">Todos</MenuItem>
+    <MenuItem value="Pendiente">Pendiente</MenuItem>
+    <MenuItem value="Cancelado">Cancelado</MenuItem>
+    <MenuItem value="Completado">Completado</MenuItem>
+  </Select>
+
+
+  {/* Tabla de Transacciones */}
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>ID Venta</TableCell>
+          <TableCell>Monto</TableCell>
+          <TableCell>Fecha Inicio</TableCell>
+          <TableCell>Fecha Fin</TableCell>
+          <TableCell>Medio Pago</TableCell>
+          <TableCell>Estado</TableCell>
+        </TableRow>
+      </TableHead>
+      {/* ... aquí iría la tabla de transacciones y filtros  ... */}
+
+
+      <TableBody>
+        {filteredTransactions.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell>{row.id}</TableCell>
+            <TableCell>{row.monto}</TableCell>
+            <TableCell>{row.fechaInicio}</TableCell>
+            <TableCell>{row.fechaFin}</TableCell>
+            <TableCell>{row.medioPago}</TableCell>
+            <TableCell>{row.estado}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+
+
+
+
+  {/* Gráficas de Pie */}
+  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    {/* Gráfica de Pie por Estados */}
+    <PieChart width={400} height={400}>
+      <Pie
+        data={dataGroupedByState}
+        cx={200}
+        cy={200}
+        labelLine={false}
+        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {dataGroupedByState.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={paymentStatusColors[entry.name]} />
+        ))}
+      </Pie>
+      <Legend />
+      <Tooltip />
+    </PieChart>
+
+
+    {/* Gráfica de Pie por Métodos de Pago */}
+    <PieChart width={400} height={400}>
+      <Pie
+        data={dataGroupedByPaymentMethod}
+        cx={200}
+        cy={200}
+        labelLine={false}
+        label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {dataGroupedByPaymentMethod.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={paymentStatusColors[entry.name] || "#8884d8"} />
+        ))}
+      </Pie>
+      <Legend />
+      <Tooltip />
+    </PieChart>
+  </div>
+</Paper>
+</div>
+);
+
+
+};  
+
+export default ShoppingCartPage;
