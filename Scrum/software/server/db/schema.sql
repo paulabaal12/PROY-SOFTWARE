628b610a72b66bc11234e4af68747c179549fa78
gustavo <cru22779@uvg.edu.gt>
2024-07-31 22:39:14 -0600

    Backend restructure

--- Scrum/software/server/db/schema.sql
+++ Scrum/software/server/db/schema.sql
@@ -1,7 +1,8 @@
 \c users_db
 
+-- Sección 1: Para crear nuevas tablas, no agregar ningun tipo de constraint
 CREATE TABLE IF NOT EXISTS usuarios (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -15,7 +16,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 
 CREATE TABLE IF NOT EXISTS productos (
-    id SERIAL PRIMARY KEY,
+    id SERIAL,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
@@ -26,30 +27,28 @@ CREATE TABLE IF NOT EXISTS productos (
 );
 
 CREATE TABLE IF NOT EXISTS vendedores (
-    vendedor_id SERIAL PRIMARY KEY,
+    vendedor_id SERIAL,
     nombre_vendedor VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ventas (
-    id_venta SERIAL PRIMARY KEY,
+    id_venta SERIAL,
     vendedor_id INT,
     monto DECIMAL(10, 2) NOT NULL,
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE,
     medio_pago VARCHAR(50),
     estado VARCHAR(50)
-    -- FOREIGN KEY (vendedor_id) REFERENCES vendedores (vendedor_id)
 );
 
 CREATE TABLE IF NOT EXISTS pedidos (
-    id_pedido SERIAL PRIMARY KEY,
+    id_pedido SERIAL,
     usuario_id INT NOT NULL,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     estado VARCHAR(50) DEFAULT 'pendiente',
     total DECIMAL(10, 2) NOT NULL,
     detalles JSONB,
     direccion_id INT
-    -- FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
 );
 
 CREATE TABLE IF NOT EXISTS direcciones (
@@ -57,6 +56,20 @@ CREATE TABLE IF NOT EXISTS direcciones (
     usuario_id INT,
     direccion_inicio TEXT,
     direccion_entrega TEXT
-    -- FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
 );
+
+-- Sección 2: Constraints, aqui pueden agregar toda la estructura y relacion entre las tablas
+ALTER TABLE usuarios ADD PRIMARY KEY (id);
+
+ALTER TABLE productos ADD PRIMARY KEY (id);
+
+ALTER TABLE vendedores ADD PRIMARY KEY (vendedor_id);
+
+ALTER TABLE ventas ADD PRIMARY KEY (id_venta);
+ALTER TABLE ventas ADD FOREIGN KEY (vendedor_id) REFERENCES vendedores (vendedor_id);
+
+ALTER TABLE pedidos ADD PRIMARY KEY (id_pedido);
+ALTER TABLE pedidos ADD FOREIGN KEY (usuario_id) REFERENCES usuarios (id);
+
+ALTER TABLE direcciones ADD FOREIGN KEY (usuario_id) REFERENCES usuarios (id);
 
