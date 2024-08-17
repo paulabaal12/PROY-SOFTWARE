52290cd99f70ce1b957e9a35f0af4a6f0ad3718b
gustavo <cru22779@uvg.edu.gt>
2024-08-16 20:59:14 -0600

    Added new tables/attributes to DB

--- Scrum/software/server/db/schema.sql
+++ Scrum/software/server/db/schema.sql
@@ -23,7 +23,9 @@ CREATE TABLE IF NOT EXISTS productos (
     categoria VARCHAR(255) NOT NULL,
     estado VARCHAR(255) NOT NULL,
     imagen_principal VARCHAR(255),
-    imagenes_adicionales TEXT[]
+    imagenes_adicionales TEXT[],
+	peso DECIMAL(10,2),
+	dimensiones VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS vendedores (
@@ -48,7 +50,10 @@ CREATE TABLE IF NOT EXISTS pedidos (
     estado VARCHAR(50) DEFAULT 'pendiente',
     total DECIMAL(10, 2) NOT NULL,
     detalles JSONB,
-    direccion_id INT
+    direccion_id INT,
+	costo_envio DECIMAL(10,2),
+	timepo_estimado_entrega TIMESTAMP,
+	estado_envio VARCHAR DEFAULT 'En proceso...'
 );
 
 CREATE TABLE IF NOT EXISTS direcciones (
@@ -58,12 +63,22 @@ CREATE TABLE IF NOT EXISTS direcciones (
     direccion_entrega TEXT
 );
 
+CREATE TABLE IF NOT EXISTS envios (
+	id_envio SERIAL,
+	pedido_id INT NOT NULL,
+	proveedor_envio VARCHAR(64) NOT NULL,
+	numero_rastreo VARCHAR(32),
+	fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	estado_envio VARCHAR(50) DEFAULT 'En tránsito',
+)
+
 -- Sección 2: Constraints, aqui pueden agregar toda la estructura y relacion entre las tablas
 ALTER TABLE usuarios ADD PRIMARY KEY (id);
 
 ALTER TABLE productos ADD PRIMARY KEY (id);
 
 ALTER TABLE vendedores ADD PRIMARY KEY (vendedor_id);
+ALTER TABLE envios ADD PRIMARY KEY (id_envio);
 
 ALTER TABLE ventas ADD PRIMARY KEY (id_venta);
 ALTER TABLE ventas ADD FOREIGN KEY (vendedor_id) REFERENCES vendedores (vendedor_id);
@@ -72,4 +87,5 @@ ALTER TABLE pedidos ADD PRIMARY KEY (id_pedido);
 ALTER TABLE pedidos ADD FOREIGN KEY (usuario_id) REFERENCES usuarios (id);
 
 ALTER TABLE direcciones ADD FOREIGN KEY (usuario_id) REFERENCES usuarios (id);
+ALTER TABLE pedidos ADD FOREIGN KEY (pedido_id) REFERENCES pedidos (id_pedido);
 
