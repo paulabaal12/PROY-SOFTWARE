b2434560a49c6d8eed34d10724d2a126077bc5f7
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-09-26 23:10:25 -0600

    fix: Metdodo de pago
    Se elimino el metodo de pago directo con tarjeta al tener problemas de verificación, se dejo el paypal

--- Scrum/software/imports/ui/Components/Screens/PaymentMethodPage.jsx
+++ Scrum/software/imports/ui/Components/Screens/PaymentMethodPage.jsx
@@ -101,16 +101,7 @@ const PaymentMethodPage = () => {
           <h1>Seleccione su Método de Pago</h1>
           <form onSubmit={handleSubmit}>
             <div>
-              <label>
-                <input
-                  type="radio"
-                  name="paymentType"
-                  value="creditCard"
-                  checked={paymentType === 'creditCard'}
-                  onChange={handlePaymentTypeChange}
-                />
-                Tarjeta de Crédito
-          </label>
+
           <label>
             <input
               type="radio"
@@ -124,51 +115,6 @@ const PaymentMethodPage = () => {
         </div>
         {paymentType === 'creditCard' && (
           <div>
-            <label>
-              Número de Tarjeta:
-              <input
-                type="text"
-                name="cardNumber"
-                value={cardDetails.cardNumber}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Nombre del Titular:
-              <input
-                type="text"
-                name="cardHolder"
-                value={cardDetails.cardHolder}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Mes de Expiración:
-              <input
-                type="text"
-                name="expiryMonth"
-                value={cardDetails.expiryMonth}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              Año de Expiración:
-              <input
-                type="text"
-                name="expiryYear"
-                value={cardDetails.expiryYear}
-                onChange={handleInputChange}
-              />
-            </label>
-            <label>
-              CVV:
-              <input
-                type="text"
-                name="cvv"
-                value={cardDetails.cvv}
-                onChange={handleInputChange}
-              />
-            </label>
           </div>
         )}
           {paymentType === 'paypal' && (
