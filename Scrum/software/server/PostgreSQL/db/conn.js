69642b4c002d248ea0788f660ee165694e7ed1dd
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-11-01 00:26:32 -0600

    update data

--- Scrum/software/server/PostgreSQL/db/conn.js
+++ Scrum/software/server/PostgreSQL/db/conn.js
@@ -4,9 +4,9 @@ import { Meteor } from 'meteor/meteor';
 const pgConfig = Meteor.settings.postgres || {
   host: 'localhost',
   port: 5432,
-  database: 'softA3',
-  user: 'postgres',
-  password: '1234'
+  database: 'data_base',
+  user: 'user',
+  password: 'password'
 };
 
 export const pool = new Pool(pgConfig);
